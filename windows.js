02567a1f1c0e8fbe26a0aebf9d79795a8209c625
Jasper <114035580+Jaspvr@users.noreply.github.com>
2023-02-17 08:55:52 -0800

    Add files via upload

--- windows.js
+++ windows.js
@@ -15,30 +15,28 @@ var dy = 6;
 var radius = 30;
 var count = 1;
 var count2 = 0;
+var count3 = 0;
+
+var holex = 500;
+var holey = 100;
+var holer = 50;
 
 function animate() {
 	requestAnimationFrame(animate);
 	//clear the canvas (to have one circle at a time)
 	if(count<10){
 	c.clearRect(0, 0, innerWidth, innerHeight);
-	c.fillStyle = 'green';
+	c.fillStyle = 'black';
 	c.fillRect(0, 0, 4000, 40000);
 	}
-	else if(count>=10 && count<20){
-		c.clearRect(0, 0, innerWidth, innerHeight);
-		c.fillStyle = 'green';
-		c.fillRect(0, 0, 4000, 40000);
-		c.arc(400, 400, 2, 0, Math.PI * 2, false);
-		c.fillStlye = 'black';
-		c.stroke();
-		c.fill();
-	}else{
+	
+	else{
 		c.clearRect(0, 0, innerWidth, innerHeight);
 		c.fillStyle = 'green';
 		c.fillRect(0, 0, 4000, 40000);
 		c.beginPath();
 		c.fillStyle = 'black';
-		c.arc(500, 100, 50, 0, Math.PI * 2, false);
+		c.arc(holex, holey, holer, 0, Math.PI * 2, false);
 		c.fillStlye = 'black';
 		c.stroke();
 		c.fill();
@@ -46,7 +44,7 @@ function animate() {
 	
 
 	//change colour
-	if(count%2 ==0 && count<20){
+	if(count%2 ==0 && count<10){
 		c.beginPath();
 		c.fillStyle = 'red';
 		c.arc(x, y, radius, 0, Math.PI * 2, false);
@@ -55,15 +53,25 @@ function animate() {
 		c.fill();
 	}
 	//change colour
-	else if(count%3 == 0 && count<20){
+	else if(count%3 == 0 && count<10){
 		c.beginPath();
 		c.fillStyle = 'pink';
 		c.arc(x, y, radius, 0, Math.PI * 2, false);
 		c.strokeStlye = 'black';
 		c.stroke();
 		c.fill();
+	//ball in hole
+	}else if(count>20 && (x+radius)<(holex+holer) && (y+radius)<(holey+holer) && 
+		(x-radius)>(holex-holer) && (y+radius)<(holey+holer)){
+			//do nothing
+		count3++;
+	}else if(count>20 && (x+radius)<(holex+holer) && (y+radius)<(holey+holer) && 
+		(x-radius)>(holex-holer) && (y+radius)<(holey+holer)){
+	//do nothing
+count3++;
+	}
 	//change colour
-	}else{
+	else if(count3 ==0){
 		c.beginPath();
 		c.fillStyle = 'white';
 		c.arc(x, y, radius, 0, Math.PI * 2, false);
@@ -72,6 +80,7 @@ function animate() {
 		c.fill();
 	}
 
+	
 	if( (x + radius) > innerWidth || (x-radius) < 0){
 		dx = -dx;
 		//change direction
